d24813fe70813a17b26205ea6a4720da32b4a012
Sho Ando <spdtas0223@gmail.com>
2020-09-11 15:53:10 +0900

    ユーザー認証機能

--- chat/urls.py
+++ chat/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
   path('', views.index , name="index"),
+  path('accounts/signup/', views.SignUpView.as_view(), name="signup"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth import login
+from django.http import HttpResponseRedirect
+from django.contrib.auth.forms import UserCreationForm
 
 
+@login_required
 def index(request):
   return render(request, 'chat/index.html')
-  
 
+class SignUpView(generic.CreateView):
+  form_class = UserCreationForm
+  success_url = reverse_lazy('login')
+  template_name = 'registration/signup.html'
